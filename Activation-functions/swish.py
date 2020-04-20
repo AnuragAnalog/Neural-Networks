7c0a9dd039a63169a853b42e562c17b12898aa5e
Krishna Desabhotla <37619724+Cquential@users.noreply.github.com>
2020-04-20 11:42:42 +0530

    Create swish.py
    Swish_function added

---
+++ Activation-functions/swish.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+
+from math import exp
+
+
+
+def swish(a: float) -> float:
+
+    """
+
+        Swish function, which has a ramp shape, it defines the positive part of the argument.
+
+
+
+        Arguments
+
+        ---------
+
+            z: float value
+
+
+
+        Returns
+
+        -------
+
+            Float value
+
+    """
+    sigmoid = 1/(1+exp(-x)
+
+
+    return x*sigmoid
